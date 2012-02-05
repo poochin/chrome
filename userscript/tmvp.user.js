fa8105a7a16a2b99b33d1e5aab5b137bc5503c08
Seitarou <fg2qcp@gmail.com>
2012-02-05 13:33:21 +0900

    oldestの際にも一応オフセット的なものを含めるように。そのオフセットを認識しませんが。

--- userscript/tmvp.user.js
+++ userscript/tmvp.user.js
@@ -7,56 +7,61 @@
 // @author      poochin
 // @license     MIT
 // @updated     2012-02-04
-// @updateURL   https://github.com/poochin/chrome/raw/master/userscript/
+// @updateURL   https://github.com/poochin/chrome/raw/master/userscript/tmvp.user.js
 // ==/UserScript==
 
-var script = '(function() {\n'
-        +'    _pc = ViewManager.prototype.putCell;\n'
-        +'    ViewManager.prototype.putCell = function (_img) {\n'
-        +'        _pc.apply(this, [_img]);\n'
-        +'        _req = this.ILC.ARC.onlyRequest;\n'
-        +'        sign = _req.type == "oldest" ? "-" : "+";\n'
-        +'        h = ["!", _req.name, "+" + _req.offset].join("/");\n'
-        +'        location.hash = h;\n'
-        +'        UrlHashManager.beforeHash = location.hash;\n'
-        +'    }\n'
-        +'})()\n'
-        +'\n'
-        +'UrlHashManager.refreshPage = function () {\n'
-        +'    this.beforeHash = location.hash;\n'
-        +'    this.oldCheck();\n'
-        +'    var hashObj = this.getHashToObj();\n'
-        +'\n'
-        +'    if ("!" == hashObj[0]) {\n'
-        +'\n'
-        +'        hashObj[hashObj.length - 1] == "250px" ? this.setImageSize(250) : this.setImageSize();\n'
-        +'        if ("perma_list" == hashObj[1]) {\n'
-        +'            if (!hashObj[2]) location.href = CONST.BASE_URL;\n'
-        +'            PageManager.setPage(this.decodeSummary(hashObj[2]), "PermaList", hashObj[3]);\n'
-        +'        } else {\n'
-        +'            var _summary = {};\n'
-        +'            _summary.name = hashObj[1];\n'
-        +'            _summary.type = undefined;\n'
-        +'            for (var i = 0; i < hashObj.length; ++i) {\n'
-        +'                h = hashObj[i];\n'
-        +'                switch (h[0]) {\n'
-        +'                    case "#": _summary.tag = h.slice(1); break;\n'
-        +'                    case "+": _summary.offset = parseInt(h.slice(1)); break;\n'
-        +'                    case "-": _summary.offset = parseInt(h.slice(1)); _summary.type = "oldest"; break;\n'
-        +'                    default: break;\n'
-        +'                }\n'
-        +'            }\n'
-        +'            PageManager.setPage([_summary], "Blog");\n'
-        +'        }\n'
-        +'    } else {\n'
-        +'        PageManager.setPage(cookieManager.dataObj.summary);\n'
-        +'    }\n'
-        +'}\n'
-        +'UrlHashManager.refreshPage();';
+var script = function () {
+    _pc = ViewManager.prototype.putCell;
+    ViewManager.prototype.putCell = function (_img) {
+        _pc.apply(this, [_img]);
+        _req = this.ILC.ARC.onlyRequest;
+        sign = (_req.type == "oldest" ? "-" : "+");
+        offset = (_req.type == "oldest" ? _req.oldestStart : _req.offset);
+        h = ["!", _req.name, sign + offset].join("/");
+        if (location.hash == UrlHashManager.beforeHash) {
+            location.hash = h;
+            UrlHashManager.beforeHash = location.hash;
+        }
+    }
+    UrlHashManager.refreshPage = function () {
+        this.beforeHash = location.hash;
+        this.oldCheck();
+        var hashObj = this.getHashToObj();
+    
+        if ("!" == hashObj[0]) {
+            hashObj[hashObj.length - 1] == "250px" ? this.setImageSize(250) : this.setImageSize();
+            if ("perma_list" == hashObj[1]) {
+                if (!hashObj[2]) location.href = CONST.BASE_URL;
+                PageManager.setPage(this.decodeSummary(hashObj[2]), "PermaList", hashObj[3]);
+            } else {
+                var _summary = {};
+                _summary.name = hashObj[1];
+                _summary.type = undefined;
+                for (var i = 0; i < hashObj.length; ++i) {
+                    h = hashObj[i];
+                    switch (h[0]) {
+                        case "#": _summary.tag = h.slice(1); break;
+                        case "+": _summary.offset = parseInt(h.slice(1)); break;
+                        case "-": _summary.offset = parseInt(h.slice(1)); _summary.type = "oldest"; break;
+                        default: break;
+                    }
+                    switch (h) {
+                        case "oldest": _summary.type = "oldest"; break;
+                        default: break;
+                    }
+                }
+                PageManager.setPage([_summary], "Blog");
+            }
+        } else {
+            PageManager.setPage(cookieManager.dataObj.summary);
+        }
+    }
+    UrlHashManager.refreshPage();
+};
 
 function main() {
     elm = window.document.createElement('script');
-    elm.innerHTML = script;
+    elm.innerHTML = '(' + script.toString() + ')()';
     console.log(elm);
     window.document.body.appendChild(elm);
 }
