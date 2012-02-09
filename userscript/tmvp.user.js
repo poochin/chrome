d04a6d81fd3035ca352541610bfd3a5afa58733b
Seitarou <fg2qcp@gmail.com>
2012-02-09 15:28:05 +0900

    newest/oldest以外を選んだ際にもハッシュの上書きをしてしまう不具合を解消。また Firefox でアドオンを多く入れているとlocation.hashへの代入が重くなるため、オフセット値の推移をobserve化しました。

--- userscript/tmvp.user.js
+++ userscript/tmvp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr Mosaic Viewer p'Plus
 // @match       http://tmv.proto.jp/*
-// @version     1.0.0
+// @version     1.0.1
 // @description Tumblr Mosaic Viewer でオフセットオプションが動くようにします
 // 
 // @author      poochin
@@ -11,35 +11,17 @@
 // ==/UserScript==
 
 var script = function () {
-    /* Proxy: ViewManager.putCell */
-    var _pc = ViewManager.prototype.putCell;
-    ViewManager.prototype.putCell = function (_img) {
-        _pc.apply(this, [_img]);
-        if (location.hash == UrlHashManager.beforeHash) {
-            _req = this.ILC.ARC.onlyRequest;
-            sign = (_req.type == "oldest" ? "-" : "+");
-            offset = (_req.type == "oldest" ? _req.oldestStart : _req.offset);
-            lencache = (this.ILC.imagesList.length + this.ILC.ARC.objectList.length);
-            offset -= parseInt(sign + '1') * lencache;
-            h = ["!", _req.name, sign + offset].join("/");
-            location.hash = h;
-            UrlHashManager.beforeHash = location.hash;
-        }
-    };
-    /* 派生: ApiReadRequest */
+    /* ApiReadRequest に関数をかませて oldestStart 初期化に対応します */
     var _ar = ApiReadRequest;
     ApiReadRequest = function(_Summary, _ApiReadController) {
         if (_Summary.oldestStart) {
             this.getNextOldestFirst = this.getNextOldest;
         }
-        else {
-            this.getNextOldestFirst = _ar.prototype.getNextOldestFirst;
-        }
         _ar.apply(this, [_Summary, _ApiReadController]);
         this.oldestStart = _Summary.oldestStart || 0;
     };
     ApiReadRequest.prototype = _ar.prototype;
-
+    /* ハッシュにおけるオフセット値の入力に対応します */
     UrlHashManager.refreshPage = function () {
         this.beforeHash = location.hash;
         this.oldCheck();
@@ -63,7 +45,9 @@ var script = function () {
                         default: break;
                     }
                     switch (h) {
-                        case "oldest": _summary.type = "oldest"; break;
+                        case "oldest": /* fall threw */
+                        case "random": /* fall trhew */
+                        case "around": _summary.type = h; break;
                         default: break;
                     }
                 }
@@ -72,13 +56,41 @@ var script = function () {
         } else {
             PageManager.setPage(cookieManager.dataObj.summary);
         }
-    }
+    };
+    /* オフセット値を監視します */
+    function buildOffset() {
+        var VM = CONST.MOS_ELEM.VM;
+        var req = VM.ILC.ARC.onlyRequest;
+        var sign = (req.type == "oldest" ? "-" : "+"),
+            offset = (req.type == "oldest" ? req.oldestStart : req.offset),
+            lencache = (VM.ILC.imagesList.length + VM.ILC.ARC.objectList.length);
+        offset -= parseInt(sign + "1") * lencache;
+        return sign + offset;
+    };
+    UrlHashManager.observeArcOffset = function() {
+        var blankspan = 100;
+        var VM = CONST.MOS_ELEM.VM;
+        if (VM.ILC.ARC.onlyRequest && location.hash == UrlHashManager.beforeHash) {
+            var hashObj = UrlHashManager.getHashToObj();
+            var req = CONST.MOS_ELEM.VM.ILC.ARC.onlyRequest;
+            var offset = buildOffset();
+            if ((req.type == "newest" || req.type == "oldest") && hashObj.indexOf(offset) == -1) {
+                h = ["!", req.name, offset].join("/");
+                location.hash = h;
+                UrlHashManager.beforeHash = location.hash;
+                blankspan = 400;
+            }
+        }
+        setTimeout(arguments.callee, blankspan);
+    };
+    /* 先に refreshPage 呼び出さないとハッシュが上書きされてしまいます */
     UrlHashManager.refreshPage();
+    UrlHashManager.observeArcOffset();
 };
 
 function main() {
-    elm = window.document.createElement('script');
-    elm.innerHTML = '(' + script.toString() + ')()';
+    elm = window.document.createElement("script");
+    elm.innerHTML = "(" + script.toString() + ")()";
     /* console.log(elm); */
     window.document.body.appendChild(elm);
 }
@@ -87,5 +99,5 @@ if (window.document.body) {
     main()
 }
 else {
-    window.document.addEventListener('DOMContentLoaded', main, false);
+    window.document.addEventListener("DOMContentLoaded", main, false);
 }
