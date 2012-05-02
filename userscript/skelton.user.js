912efc49143448da95c48d5d991679b9e29078be
Seitarou <fg2qcp@gmail.com>
2012-05-02 11:56:52 +0900

    UserScript の雛形

---
+++ userscript/skelton.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        UserScript Skelton
+// @match       http://*/*
+// @version     1.0.0
+// @description ユーザスクリプトの概要を記入してください
+// 
+// @author      poochin
+// @license     MIT
+// @updated     1970-01-01
+// @updateURL   https://github.com/poochin/
+// ==/UserScript==
+
+function main() {
+}
+
+function isExecPage() {
+    if (/^https?:\/\/www\.tumblr\.com\//.test(location) /* for Opera */) {
+        return true;
+    }
+}
+
+if (window.document.body) {
+    if (isExecPage() /* for Opera */) {
+        main();
+    }
+}
+else {
+    window.document.addEventListener('DOMContentLoaded', main, false);
+}
+
